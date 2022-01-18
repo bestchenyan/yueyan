7ce8a9c5b1cdde8109907bfa3c6ae92eadbbf06c
chenyan <chenyan@zhitongits.com.cn>
2022-01-18 13:44:06 +0800

    模拟数据

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import router from "./router";
 import store from "./store";
+import VueLazyload from "vue-lazyload";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
 
 import "@/assets/styles";
 import Antd from "ant-design-vue";
@@ -10,7 +13,22 @@ dayjs.locale(zhcn);
 
 Vue.config.productionTip = false;
 Vue.use(Antd);
-
+Vue.use(VueAwesomeSwiper);
+Vue.use(VueLazyload, {
+    preLoad: 1.3,
+    error: require("@/assets/image/no-data.png"),
+    loading: require("@/assets/image/loading.gif"),
+    attempt: 1,
+    filter: {
+        webp(listener: any, options: any) {
+            if (!options.supportWebp) return;
+            if (listener.src === null) listener.src = "";
+            if (listener.src) {
+                listener.src = "https://yueyan-1306269746.cos.ap-nanjing.myqcloud.com/img" + listener.src;
+            }
+        },
+    },
+});
 new Vue({
     router,
     store,
--- src/views/app/home/index.ts
+++ src/views/app/home/index.ts
@@ -1,8 +1,11 @@
 import { Vue, Component } from "vue-property-decorator";
 import Amap from "@/components/Amap";
+
 import "./index.scss";
 @Component({
     template: require("./index.html"),
     components: { Amap },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+    videoUrl = "https://yueyan-1306269746.cos.ap-nanjing.myqcloud.com/video/yulanyayuan/116773.mp4";
+}
